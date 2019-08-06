18e915750982ed651866b4ba2dde140416b8435d
koge <koge.k@dentsudigital.co.jp>
2019-08-06 14:32:16 +0900

    no message

--- index.php
+++ index.php
@@ -16,6 +16,15 @@ if ( is_writable ( ABSPATH . 'manifest.json' ) || !file_exists ( ABSPATH . 'mani
   // display an error
 }
 */
+    $myclient = new ET_Client();
+    $dataExtensionExternalKey = getenv('PUSH_REGISTRATION_DE_EXTERNAL_KEY_01');
+    $dataExtensionName = getenv('PUSH_REGISTRATION_DE_NAME_01');
+
+var_dump($dataExtensionExternalKey, $dataExtensionName);
+
+
+
+
 ?>
 <html lang="ja">
   <head>
